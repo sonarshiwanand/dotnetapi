4dcf7a9cdbf2d54477547f9540df0ee3a91f3ded
sonar <sonar.shiwanand@gmail.com>
2021-10-10 23:10:02 +0530

    first commit

---
+++ shoppingclientapi/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using shoppingclientapi.Data;
+using shoppingclientapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shoppingclientapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
---
+++ shoppingclientapi/Data/ProductContext.cs
@@ -0,0 +1,30 @@
+﻿using shoppingclientapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shoppingclientapi.Data
+{
+    public static class ProductContext
+    {
+
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product(){
+                Name = "Nokia",
+                Description ="Nokia Phone",
+                ImageFile="nokia.png",
+                Price = 100.00M,
+                Category="Phone"
+            },
+            new Product(){
+                Name = "Samsung",
+                Description ="Samsung Phone",
+                ImageFile="samsung.png",
+                Price = 100.00M,
+                Category="Phone"
+            }
+        };
+    }
+}
